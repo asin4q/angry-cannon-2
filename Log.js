93a953bb125f0b0e9f41d8d377a3531ecfb695ce
asin4q <122594231+asin4q@users.noreply.github.com>
2023-06-12 17:53:15 +1000

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,11 @@
+class log extends BaseClass {
+    constructor(x,y,width,height,angle){
+        super(x,y,width,height)
+        this.angle = angle
+
+        
+        this.image = loadImage("assets/wood2.png")
+
+
+    }
+}
